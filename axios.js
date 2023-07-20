3e178201362200a6ef9b034817918ded7730c08f
giuseppe serna <giuseppe.serna@polimi.it>
2023-07-20 18:13:42 +0200

    add axios for deploy configuration

---
+++ axios.js
@@ -0,0 +1,23 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+// to work in local use instead this ip
+const api = axios.create({ baseURL: 'http://localhost:51119' })
+const patientSearchApi = axios.create({ baseURL: 'http://localhost:51125' })
+
+
+// const api = axios.create({ baseURL: 'http://131.175.15.22:61111/hbd-demo-api/' })
+// const patientSearchApi = axios.create({baseURL: 'http://131.175.15.22:61111/patient-search-server/'})
+export default boot(({ app }) => {
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
+  app.config.globalProperties.$api = api
+  app.config.globalProperties.$api = patientSearchApi
+
+})
+
+export { axios, api, patientSearchApi}
