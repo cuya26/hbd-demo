3e9858ef17bd69cd185d9f134f58a8385b9f8bda
Raffaello Fornasiere <fornasiere.raffaello@gmail.com>
2023-12-28 08:12:01 +0100

    refactor fetch

--- frontend/src/components/MedicalInformationExtraction.vue
+++ frontend/src/components/MedicalInformationExtraction.vue
@@ -1,8 +1,8 @@
 <script>
 import {ref} from "vue";
 import {llamaHost} from "boot/axios";
-import {v4 as uuidv4} from 'uuid';
-
+import PromptComponent from "components/MedicalInformationExtraction/Prompt.vue";
+import TimelineComponent from "components/MedicalInformationExtraction/TimelineComponent.vue";
 /* prettier-ignore */
 /* @formatter:off */
  const columns = [
@@ -12,83 +12,77 @@ import {v4 as uuidv4} from 'uuid';
    {name: 'route', label: 'Route', field: 'route', align: 'left', sortable: true},
 ]
 
- const rows = [
- ]
+const rows = [
+]
 /* @formatter:on */
 export default {
   name: "MedicationExtraction",
   props: ['doc'],
   emits: ['request-document'],
+  components: {PromptComponent, TimelineComponent},
   watch: {
     doc(newValue, oldValue) {
     }
   },
   data() {
     return {
-      table: ref({
-        columns: columns,
-        rows: rows,
-      }),
-      modelParameters: {
-        max_tokens: {
-          placeholder: "Max tokens",
-          type: "number",
-          model: ref(512),
-        },
-        temperature: {
-          placeholder: "Temperature",
-          type: "float",
-          model: ref(0),
-        },
-        top_p: {
-          placeholder: "Top_p",
-          type: "float",
-          model: ref(0),
-        },
-        top_k: {
-          placeholder: "Top_k",
-          type: "number",
-          model: ref(0),
-        },
-        mirostat_tau: {
-          placeholder: "Mirostat_tau",
-          type: "float",
-          model: ref(3.0),
-        },
-        repeat_penalty: {
-          placeholder: "Repeat penalty",
-          type: "float",
-          model: ref(1.1),
-        }
-      },
-      editaleTable: ref(false),
-      answer: ref(`
-      \nAdalat 200 mg p.o. b.i.d. || 200 mg || p.o. || b.i.d.\nZantac 150 mg p.o. b.i.d. || 150 mg || p.o. || b.i.d.\nMagnesium Oxide 40 mg t.i.d. || 40 mg || t.i.d. || nm\nUltram 300 mg q.d. || 300 mg || q.d. || nm\nTrazodone 100 mg q.h.s. || 100 mg || q.h.s. || nm\nAzmacort 80 mg p.r.n. || 80 mg || p.r.n. || nm\naspirin 81 mg q.d. || 81 mg || q.d. || nm\nDyazide 25 mg q.d. || 25 mg || q.d. || nm\nnose spray b.i.d. || nm || nm || nm\ncalcium chloride pills q.d. || nm || nm || nm\nColchicine 600 mg q.d. || 600 mg || q.d. || nm\ncyproheptadine hydrochloride 4 mg b.i.d. q.h.s. || 4 mg || b.i.d. || nm\nanticholesterol med. || nm || nm || nm\nAlbuterol nebulizers 250 mg q.4h. || 250 mg || q.4h. || nm\nAllopurinol 300 mg q.d. || 300 mg || q.d. || nm\nColchicine 0.6 mg q.d. || 0.6 mg || q.d. || nm\ncyproheptadine hydrochloride by mouth 400 mg q.d. || 400 mg || q.d. || nm\nDigoxin 0.125 mg q.d. || 0.125 mg || q.d. || nm\nDiltiazem 30 mg t.i.d. || 30 mg || t.i.d. || nm\nLasix 40 mg p.o. q.d. || 40 mg || p.o. || q.d.\nPercocet 1-2 tablets p.o. q.4h. p.r.n. || 1-2 tablets || p.o. || q.4h. p.r.n.\nDilantin 200 mg p.o. b.i.d. || 200 mg || p.o. || b.i.d.\nTrazodone 100 mg p.o. q.h.s. || 100 mg || p.o. || q.h.s.\n[CSV_1_END]`),
-      tab: ref('table'),
-      loadingResponse: ref(false),
-      showTemplate: ref(false),
-      completionInit: ref('[CSV_1_START]\n' +
-        'medication || dosage || mode || frequency'),
-
-      prompt: ref(`TASK: Named Entity Medical Extraction
+      medExt: {
+        answer: '',
+        completionInit: '[CSV_1_START]\n' +
+          'medication || dosage || mode || frequency',
+        userMessage: `TASK: Named Entity Medical Extraction
 Extract all and only the medication, dosage, mode, frequency from the text.
 Put all the extracted entities into a csv table with the columns medication and dosage.
 If no data in any of the columns write "nm".
 Follow a csv format, like this "medication || dosage || mode || frequency".
 [CLINICAL_DOCUMENT_START]
 {file}
-[CLINICAL_DOCUMENT_END]`),
+[CLINICAL_DOCUMENT_END]`,
+        systemMessage: `Emulate a tool for the specified task. Strictly follow the provided instructions to execute the task accurately. Your role is to adhere solely to the given guidelines and perform the designated actions without deviation.`,
+      },
+      timeline:{
+        answer: '',
+        completionInit: '',
+        userMessage: `
+TASK: Temporal Entity Extraction from Medical Document
+Extract a coherent chronological sequence of events from the medical document. Use each date mentioned in the text as a timeline delimiter, emphasizing the context for events without specific dates. Ensure that the output is a list of events ordered chronologically, with each event clearly associated with its relative position in the timeline. Be explicit about the importance of maintaining chronological coherence among the events.
+
+The output must be in JSON list of objects where each object contains the "date" key and the  "events" key.
+If there is no date (like before admission), put "none"
+Example:
+
+[{
+ "date": "10/07/1998",
+ "events": ["presents with abdominal pain of seven days duration", "admission", "diagnosed with acute pancreatitis"]
+}, ... ]
+
+[CLINICAL_DOCUMENT_START]
+{file}
+[CLINICAL_DOCUMENT_END]
+`,
+        systemMessage: `Emulate a tool for the specified task. Strictly follow the provided instructions to execute the task accurately. Your role is to adhere solely to the given guidelines and perform the designated actions without deviation.`,
+      },
+      table: ref({
+        columns: columns,
+        rows: rows,
+      }),
+      editaleTable: ref(false),
+      answer: ref(`
+      \nAdalat 200 mg p.o. b.i.d. || 200 mg || p.o. || b.i.d.\nZantac 150 mg p.o. b.i.d. || 150 mg || p.o. || b.i.d.\nMagnesium Oxide 40 mg t.i.d. || 40 mg || t.i.d. || nm\nUltram 300 mg q.d. || 300 mg || q.d. || nm\nTrazodone 100 mg q.h.s. || 100 mg || q.h.s. || nm\nAzmacort 80 mg p.r.n. || 80 mg || p.r.n. || nm\naspirin 81 mg q.d. || 81 mg || q.d. || nm\nDyazide 25 mg q.d. || 25 mg || q.d. || nm\nnose spray b.i.d. || nm || nm || nm\ncalcium chloride pills q.d. || nm || nm || nm\nColchicine 600 mg q.d. || 600 mg || q.d. || nm\ncyproheptadine hydrochloride 4 mg b.i.d. q.h.s. || 4 mg || b.i.d. || nm\nanticholesterol med. || nm || nm || nm\nAlbuterol nebulizers 250 mg q.4h. || 250 mg || q.4h. || nm\nAllopurinol 300 mg q.d. || 300 mg || q.d. || nm\nColchicine 0.6 mg q.d. || 0.6 mg || q.d. || nm\ncyproheptadine hydrochloride by mouth 400 mg q.d. || 400 mg || q.d. || nm\nDigoxin 0.125 mg q.d. || 0.125 mg || q.d. || nm\nDiltiazem 30 mg t.i.d. || 30 mg || t.i.d. || nm\nLasix 40 mg p.o. q.d. || 40 mg || p.o. || q.d.\nPercocet 1-2 tablets p.o. q.4h. p.r.n. || 1-2 tablets || p.o. || q.4h. p.r.n.\nDilantin 200 mg p.o. b.i.d. || 200 mg || p.o. || b.i.d.\nTrazodone 100 mg p.o. q.h.s. || 100 mg || p.o. || q.h.s.\n[CSV_1_END]`),
+      tab: ref('table'),
       template: '<|im_start|>system\n{system_message}<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n{completion_init}',
-      systemMessage: ref(`Emulate a tool for the specified task. Strictly follow the provided instructions to execute the task accurately. Your role is to adhere solely to the given guidelines and perform the designated actions without deviation.`),
+
+      loadingResponse: ref(false),
+      showTemplate: ref(false),
       shrinkPrompt: ref(false),
     }
   },
   methods: {
     adjustHeight() {
       let editableDiv = this.$refs.userMessage;
-        if (!editableDiv) return;
+      if (!editableDiv) return;
       editableDiv.style.height = 0 + 'px';
-      editableDiv.style.height =  editableDiv.scrollHeight + "px";
+      editableDiv.style.height = editableDiv.scrollHeight + "px";
     },
 
     parseAnswer(answer) {
@@ -113,88 +107,75 @@ Follow a csv format, like this "medication || dosage || mode || frequency".
       }
       this.table.rows = table
     },
-    applyToTemplate() {
-      return this.template
-        .replace('{system_message}', this.systemMessage)
-        .replace('{prompt}', this.buildUserMessage())
-        .replace('{completion_init}', this.completionInit)
-    },
-    clear() {
-      this.answer = ''
-    },
-    buildUserMessage() {
-      return this.prompt.replace('{file}', this.doc)
-    },
     prepareExtraction() {
       this.adjustHeight()
 
     },
 
-    async extractMedications() {
-      this.parseAnswer(this.answer)
-      return
-      this.loadingResponse = true
-      fetch(llamaHost + '/v1/completions', {
-        method: 'POST',
-        body: JSON.stringify({
-          prompt: this.applyToTemplate(),
-          max_tokens: 1024,
-          stream: true,
-          stop: ['<|im_end|>'],
-          temperature: this.modelParameters.temperature.model,
-          top_p: this.modelParameters.top_p.model,
-          top_k: this.modelParameters.top_k.model,
-          mirostat_tau: this.modelParameters.mirostat_tau.model,
-          repeat_penalty: 1.1,
 
-        }),
+    fetchModel(body) {
+      return fetch(llamaHost + '/v1/completions', {
+        method: 'POST',
+        body: JSON.stringify(body),
         headers: {
           'Content-Type': 'application/json',
           timeout: 36000
         }
       })
-        .then(response => {
-          const reader = response.body.getReader();
-          const processStream = ({done, value}) => {
-            if (done) {
-              this.loadingResponse = false
-              console.log('Stream complete')
-              console.log(this.answer)
-              this.parseAnswer(this.answer)
-              return;
-            }
-            let chunkRaw = new TextDecoder().decode(value);
-            const chunkArray = chunkRaw.split('data:').slice(1)
-            for (let chunk of chunkArray) {
-              try {
-                chunk = JSON.parse(chunk.split(': ping -')[0])
+    },
 
-                this.answer
-                  += chunk['choices'][0]['text']
-              } catch {
-                console.log('il parsing non è andato a buon fine')
-              }
+    mapChunk(chunkRaw) {
+      chunkRaw = new TextDecoder().decode(chunkRaw);
+      const chunkArray = chunkRaw.split('data:').slice(1)
+      let res = ''
+      for (let chunk of chunkArray) {
+        if (chunk.trim() === '[DONE]')
+          continue
+        try {
+          chunk = JSON.parse(chunk.split(': ping -')[0])
+          res
+            += chunk['choices'][0]['text']
+        } catch {
+          console.log('il parsing non è andato a buon fine', chunk)
+        }
+      }
+      return res
+    },
 
-            }
+    processMedicationExtraction(reader, {done, value}) {
+      if(done){
+        this.loadingResponse = false
+        this.parseAnswer(this.medExt.answer)
+        return;
+      }
+      let mappedChunk = this.mapChunk(value)
+      this.medExt.answer += mappedChunk
+      return reader.read().then(this.processMedicationExtraction.bind(null, reader));
+    },
 
-            return reader.read().then(processStream);
-          };
-          reader.read().then(processStream);
-        })
+    async extractMedications(data) {
+      this.loadingResponse = true
+      this.medExt.answer = ''
+      this.fetchModel(
+        {
+          prompt: data.prompt.replace('{file}', this.doc),
+          stream: true,
+          stop: ['<|im_end|>'],
+          ...data.parameters
+        }
+      ).then(response => {
+        const reader = response.body.getReader();
+        return reader.read().then(this.processMedicationExtraction.bind(null, reader));
+      })
         .catch(error => {
           console.error(error);
           this.loadingResponse = false
           return error.body;
         })
-        .catch(error => {
-          console.log(error)
-          this.loadingResponse = false
-        });
-
-
-    },
-
+    }
   },
+
+
   mounted() {
     this.prepareExtraction();
   }
@@ -225,8 +206,8 @@ Follow a csv format, like this "medication || dosage || mode || frequency".
               narrow-indicator
       >
         <q-tab name="table" label="Table"/>
-        <q-tab name="prompt" label="Prompt"/>
         <q-tab name="timeline" label="Timeline"/>
+        <q-tab name="prompt" label="Prompt"/>
       </q-tabs>
       <q-separator class="bi-border"/>
       <q-tab-panels v-model="tab" animated class="column no-wrap full-height overflow-hidden "
@@ -235,7 +216,7 @@ Follow a csv format, like this "medication || dosage || mode || frequency".
       >
         <q-tab-panel name="table" class="column full-height q-ma-none">
           <div class="flex justify-between">
-          <div class="text-h6">Table</div>
+            <div class="text-h6">Table</div>
             <q-toggle
               v-show="table.rows.length > 0"
               v-model="editaleTable"
@@ -262,26 +243,26 @@ Follow a csv format, like this "medication || dosage || mode || frequency".
                 <q-td key="name" :props="props">
                   {{ props.row.name }}
                   <q-popup-edit :disable="!editaleTable" v-model="props.row.name" v-slot="scope">
-                    <q-input v-model="scope.value" dense autofocus title="Update Name" @keyup.enter="scope.set" />
+                    <q-input v-model="scope.value" dense autofocus title="Update Name" @keyup.enter="scope.set"/>
                   </q-popup-edit>
                 </q-td>
                 <q-td key="dose" :props="props">
                   {{ props.row.dose }}
-                  <q-popup-edit :disable="!editaleTable" v-model="props.row.dose" v-slot="scope" >
-                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set" />
+                  <q-popup-edit :disable="!editaleTable" v-model="props.row.dose" v-slot="scope">
+                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set"/>
                   </q-popup-edit>
                 </q-td>
                 <q-td key="frequency" :props="props">
                   {{ props.row.frequency }}
                   <q-popup-edit :disable="!editaleTable" v-model="props.row.frequency" v-slot="scope">
-                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set" />
+                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set"/>
 
                   </q-popup-edit>
                 </q-td>
                 <q-td key="route" :props="props">
                   {{ props.row.route }}
                   <q-popup-edit :disable="!editaleTable" v-model="props.row.route" v-slot="scope">
-                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set" />
+                    <q-input v-model="scope.value" dense autofocus title="Update dosage" @keyup.enter="scope.set"/>
 
                   </q-popup-edit>
                 </q-td>
@@ -289,102 +270,21 @@ Follow a csv format, like this "medication || dosage || mode || frequency".
             </template>
           </q-table>
         </q-tab-panel>
+        <q-tab-panel name="timeline">
+          <timeline-component/>
+        </q-tab-panel>
         <q-tab-panel class="overflow-hidden full-height q-pa-none" name="prompt"
         >
-          <div class="full-height column justify-between full-width no-wrap overflow-auto">
-            <q-list class="full-height column no-wrap">
-              <q-expansion-item
-                icon="settings"
-                label="Model Settings"
-                header-class=""
-              >
-                <q-card class="q-pa-none col">
-                  <q-card-section class="q-pa-sm flex justify-start">
-                    <q-input
-                      v-for="param in modelParameters" :key="param"
-                      v-model="param.model"
-                      :type="param.type === 'float' ? 'number' : param.type"
-                      :step="param.type === 'float' ? '0.01' : ''"
-                      :label="param.placeholder"
-                      style="width: 48%"
-                      class="q-pa-none"
-                    />
-                  </q-card-section>
-                </q-card>
-              </q-expansion-item>
-
-              <q-separator/>
-
-              <q-expansion-item
-                icon="edit_note"
-                label="Prompt"
-                header-class=""
-                default-opened>
-                <q-card>
-                  <div class="bg-grey-3 overflow-scroll column full-height">
-                    <div class="col-grow flex q-pa-sm justify-end bg-grey-4 full-height">
-                      <div style="display: flex;">
-                        <q-btn class="q-mx-sm" round dense flat icon="cleaning_services" @click="clear()"/>
-                        <q-btn class="q-mx-sm" round dense flat icon="send" @click="extractMedications()"/>
-                      </div>
-                    </div>
-
-                    <div class="q-pa-sm col overflow-auto flex column">
-                      <p style="margin: 0">
-                        {{ this.template.slice(0, this.template.indexOf('{system_message}') - 1) }}</p>
-                      <textarea class="bg-white col q-pa-sm no-border rounded-borders full-width"
-                                style="resize: none"
-                                placeholder="System message"
-                                v-model="this.systemMessage"
-                      />
-                      <p style="margin: 0"
-                         v-html="this.template.slice(this.template.indexOf('{system_message}')+16, this.template.indexOf('{prompt}')-1)
-                                  .replace('\n', '<br>')"></p>
-
-                      <div class="col-grow">
-                        <textarea placeholder="User message"
-                                  @input="adjustHeight()"
-                                  class="q-pa-sm no-border rounded-borders full-width bg-white"
-                                  ref="userMessage"
-                                  v-model="this.prompt"
-                        ></textarea>
-                      </div>
-
-                      <p style="margin: 0"
-                         v-html="this.template.slice(this.template.indexOf('{prompt}')+8, this.template.indexOf('{completion_init}')-1)
-                                  .replace('\n', '<br>')"
-                      ></p>
-                      <textarea contenteditable="true" class="bg-white col q-pa-sm no-border rounded-borders full-width"
-                                style="resize: none"
-                                placeholder="Completion init"
-                                v-model="this.completionInit"
-                      />
-                    </div>
-                  </div>
-
-                </q-card>
-              </q-expansion-item>
-
-
-              <q-separator/>
-
-              <q-expansion-item
-                icon="output"
-                label="Model Output"
-              >
-                <q-card>
-                  <q-card-section style="white-space: pre-line">
-                    {{ answer }}
-                  </q-card-section>
-                </q-card>
-              </q-expansion-item>
-              <q-separator/>
-
-            </q-list>
-          </div>
+          <prompt-component
+            ref="promptComponent"
+            :template="template"
+            :answer="medExt.answer"
+            :prompt-completion-init="medExt.completionInit"
+            :prompt-system-message="medExt.systemMessage"
+            :prompt-user-message="medExt.userMessage"
+            @askLLM="extractMedications"
+          ></prompt-component>
         </q-tab-panel>
-
-
 
 
       </q-tab-panels>
---
+++ frontend/src/components/MedicalInformationExtraction/Prompt.vue
@@ -0,0 +1,186 @@
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "PromptComponent",
+  props: {
+    template: String,
+    answer: String,
+    promptSystemMessage: String,
+    promptUserMessage: String,
+    promptCompletionInit: String,
+  },
+  exposes: ['run'],
+  emits: ['askLLM', 'clearOutput'],
+
+  methods: {
+    adjustHeight(element) {
+      if (!element) return;
+      element.style.height = 0 + 'px';
+      element.style.height = element.scrollHeight + "px";
+    },
+
+    prepareData() {
+      let prompt = this.template
+        .replace('{system_message}', this.systemMessage)
+        .replace('{prompt}', this.userMessage)
+        .replace('{completion_init}', this.completionInit);
+      const parameters = Object.fromEntries(Object.entries(this.modelParameters).map(([key, value]) => [key, value.model]));
+      this.$emit('askLLM', {prompt: prompt, parameters: parameters});
+    },
+
+  },
+  data(){
+
+    return{
+      modelParameters: {
+        max_tokens: {
+          placeholder: "Max tokens",
+          type: "number",
+          model: ref(2048),
+        },
+        temperature: {
+          placeholder: "Temperature",
+          type: "float",
+          model: ref(0.5),
+        },
+        // top_p: {
+        //   placeholder: "Top_p",
+        //   type: "float",
+        //   model: ref(0),
+        // },
+        // top_k: {
+        //   placeholder: "Top_k",
+        //   type: "number",
+        //   model: ref(0),
+        // },
+        mirostat_tau: {
+          placeholder: "Mirostat_tau",
+          type: "float",
+          model: ref(8.0),
+        },
+        repeat_penalty: {
+          placeholder: "Repeat penalty",
+          type: "float",
+          model: ref(1.1),
+        }
+      },
+
+      completionInit: ref(this.promptCompletionInit),
+      userMessage: ref(this.promptUserMessage),
+      systemMessage: ref(this.promptSystemMessage),
+
+    }
+  },
+  mounted() {
+    this.adjustHeight(this.$refs.systemMessage);
+    this.adjustHeight(this.$refs.prompt);
+    this.adjustHeight(this.$refs.completionInit);
+  },
+
+}
+</script>
+
+<template>
+  <div class="full-height column justify-between full-width no-wrap overflow-auto">
+    <q-list class="full-height column no-wrap">
+      <q-expansion-item
+        icon="settings"
+        label="Model Settings"
+        header-class=""
+      >
+        <q-card class="q-pa-none col">
+          <q-card-section class="q-pa-sm flex justify-start">
+            <q-input
+              v-for="param in modelParameters" :key="param"
+              v-model="param.model"
+              :type="param.type === 'float' ? 'number' : param.type"
+              :step="param.type === 'float' ? '0.01' : ''"
+              :label="param.placeholder"
+              style="width: 48%"
+              class="q-pa-none"
+            />
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-separator/>
+
+      <q-expansion-item
+        icon="edit_note"
+        label="Prompt"
+        header-class=""
+        default-opened>
+        <q-card>
+          <div class="bg-grey-3 overflow-scroll column full-height">
+            <div class="col-grow flex q-pa-sm justify-end bg-grey-4 full-height">
+              <div style="display: flex;">
+                <q-btn class="q-mx-sm" round dense flat icon="cleaning_services" @click="$emit('clearOutput')"/>
+                <q-btn class="q-mx-sm" round dense flat icon="send" @click="prepareData()"/>
+              </div>
+            </div>
+
+            <div class="q-pa-sm col overflow-auto flex column">
+              <p style="margin: 0">
+                {{ this.template.slice(0, this.template.indexOf('{system_message}') - 1) }}</p>
+              <textarea class="bg-white col q-pa-sm no-border rounded-borders full-width"
+                        style="resize: none"
+                        ref="systemMessage"
+                        placeholder="System message"
+                        v-model="this.systemMessage"
+              />
+              <p style="margin: 0"
+                 v-html="this.template.slice(this.template.indexOf('{system_message}')+16, this.template.indexOf('{prompt}')-1)
+                                  .replace('\n', '<br>')"></p>
+
+              <div class="col-grow">
+                <textarea placeholder="User message"
+                          class="q-pa-sm no-border rounded-borders full-width bg-white"
+                          style="resize: none"
+                          ref="prompt"
+                          v-model="this.userMessage"
+                          @input="adjustHeight($event.target)"
+                ></textarea>
+              </div>
+
+              <p style="margin: 0"
+                 v-html="this.template.slice(this.template.indexOf('{prompt}')+8, this.template.indexOf('{completion_init}')-1)
+                                  .replace('\n', '<br>')"
+              ></p>
+              <textarea contenteditable="true" class="bg-white col q-pa-sm no-border rounded-borders full-width"
+                        style="resize: none"
+                        ref="completionInit"
+                        placeholder="Completion init"
+                        v-model="this.completionInit"
+                        @input="adjustHeight($event.target)"
+              />
+            </div>
+          </div>
+
+        </q-card>
+      </q-expansion-item>
+
+
+      <q-separator/>
+
+      <q-expansion-item
+        icon="output"
+        label="Model Output"
+      >
+        <q-card>
+          <q-card-section style="white-space: pre-line">
+            {{ answer }}
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <q-separator/>
+
+    </q-list>
+  </div>
+
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
